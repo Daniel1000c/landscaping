0fcd3a201a44d9aa2dcdc199f581b3efa4364a31
Daniel Velez <velezdaniel516@gmail.com>
2025-08-01 18:26:55 -0400

    delete template code and add include folders

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./src/output.css">
-    <title>Document</title>
-</head>
-<body>
-    <p>Testing</p>
-    
-</body>
+<?php
+    require('includes/header.php');
+?>
 
-</html>
+<?php
+    require('includes/footer.php');
+?>
