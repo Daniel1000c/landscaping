da2cb3b463f805b047c7fadd543e40038a598f84
Daniel Velez <velezdaniel516@gmail.com>
2025-08-02 15:45:45 -0400

    add section headers template

---
+++ includes/sectionHeader.php
@@ -0,0 +1,7 @@
+<section class="py-12">
+    <div class="container mx-auto text-center">
+        <h1 class="text-7xl font-bold text-teal-700 tracking-wide">
+            <?php echo $sectionTitle?>
+        </h1>
+    </div>
+</section>
