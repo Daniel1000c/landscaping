64256e9935f03e2438843163af991ee92d12e7cc
Daniel Velez <velezdaniel516@gmail.com>
2025-08-03 13:53:54 -0400

    add get a quote section

---
+++ includes/getAQuote.php
@@ -0,0 +1,13 @@
+<section class="py-12">
+    <div class="container mx-auto text-center">
+        <h2 class="mb-10 text-xl font-extrabold tracking-wide leading-loose text-gray-900 md:text-2xl lg:text-3xl">
+            Want To Transform Your Garden? 
+            <span class="text-green-700">Get A Quote Today!</span>
+        </h2>
+        
+        <a href="contact.php" class="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4
+         focus:ring-green-300 font-medium rounded-full text-xl px-5 py-2.5 text-center me-2 mb-2">
+          <i class='bx  bx-leaf-alt'  ></i> Get A Quote
+        </a>
+    </div>
+</section>
