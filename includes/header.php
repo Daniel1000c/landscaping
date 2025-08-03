c022e6604cae8630f22b5b0807bb81996ba1158d
Daniel Velez <velezdaniel516@gmail.com>
2025-08-03 13:52:48 -0400

    reformat header bg color

--- includes/header.php
+++ includes/header.php
@@ -5,15 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./src/output.css">
     <link rel="icon" type="image/x-icon" href="favicon/favicon.ico">
+    <link href="https://cdn.boxicons.com/fonts/basic/boxicons.min.css" rel="stylesheet">
+    <link href="https://cdn.boxicons.com/fonts/brands/boxicons-brands.min.css" rel="stylesheet">
     <title><?php echo (isset($title) ? $title . ' &#8226; ' . 'Flores Landscaping' : 'Flores Landscaping')?></title>
 </head>
 <body>
     <header>
-        <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700 font-lato tracking-widest font-mono">
+        <nav class="bg-green-700 font-lato tracking-widest font-mono">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="index.php" class="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="https://cdn-icons-png.flaticon.com/512/1565/1565245.png" class="h-8" alt="Flores Landscaping" />
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flores Landscaping</span>
+                    <span class="self-center text-md font-semibold whitespace-nowrap text-amber-100 lg:text-2xl">Flores Landscaping</span>
                 </a>
                 <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
@@ -22,7 +24,7 @@
                     </svg>
                 </button>
                 <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
-                <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
                     <li>
                     <a href="index.php" class="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Home</a>
                     </li>
