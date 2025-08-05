d4081b295c62356e6ff84e6214197199b7c535f4
Daniel Velez <velezdaniel516@gmail.com>
2025-08-05 18:38:53 -0400

    add new links to services and more info drop down menus

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,7 @@
     <title><?php echo((!empty($title)) ? $title : 'Flores Landscaping')?></title>
     <link rel="stylesheet" href="styles/style.css">
     <link rel="icon" type="image/x-icon" href="favicon/favicon.ico">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 </head>
 <body>
@@ -13,7 +14,7 @@
         <div class="container">
             <nav class="navbar navbar-expand-lg">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="index.php">Navbar</a>
+                    <a class="navbar-brand" href="index.php">Flores Landscape</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
@@ -30,9 +31,10 @@
                                 More Info
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                <li><a class="dropdown-item" href="#">Testimonials</a></li>
+                                <li><a class="dropdown-item" href="#">FAQ</a></li>
+                                <li><a class="dropdown-item" href="#">Why Choose Us</a></li>
+                                <li><a class="dropdown-item" href="#">Privacy Policy</a></li>
                             </ul>
                         </li>
                         <li class="nav-item">
@@ -43,13 +45,16 @@
                                 Services
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                <li><a class="dropdown-item" href="#">Lawn Care & Maintenance</a></li>
+                                <li><a class="dropdown-item" href="#">Landscape Design & Installation</a></li>
+                                <li><a class="dropdown-item" href="#">Hardscaping</a></li>
+                                <li><a class="dropdown-item" href="#">Tree & Shrub Care</a></li>
+                                <li><a class="dropdown-item" href="#">Irrigation systems</a></li>
+                                <li><a class="dropdown-item" href="#">Seasonal Clean-Up</a></li>
                             </ul>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Portfolio</a>
+                            <a class="nav-link" href="#">Contact Us</a>
                         </li>
                     </ul>
                     </div>
