8cd81d18e108165b969d206918dc81c564d7848f
Daniel Velez <velezdaniel516@gmail.com>
2025-08-06 14:59:18 -0400

    add get a quote section template

--- includes/getAQuote.php
+++ includes/getAQuote.php
@@ -1,5 +1,6 @@
 <section class="get-a-quote">
-    <div class="container">
-
+    <div class="container text-center p-4">
+        <h2 class="mb-4">Ready to get started? <span>Call us today!</span></h2>
+        <a href="contact.php" class="btn quote-btn">Get in Touch</a>
     </div>
 </section>
