f64f88348e05523bafc783a66d40dd18688cfbf3
Daniel Velez <velezdaniel516@gmail.com>
2025-08-06 14:34:39 -0400

    add quote button

--- includes/header.php
+++ includes/header.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo((!empty($title)) ? $title : 'Flores Landscaping')?></title>
-    <link rel="stylesheet" href="styles/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <link rel="icon" type="image/x-icon" href="favicon/favicon.ico">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
     <header>
         <div class="container">
             <nav class="navbar navbar-expand-lg">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="index.php">Flores Landscape</a>
+                    <a class="navbar-brand" href="index.php"><span class="icon-logo"><i class='bx bxs-tree'></i></span> Flores Landscape</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
@@ -56,11 +56,13 @@
                         <li class="nav-item">
                             <a class="nav-link" href="#">Contact Us</a>
                         </li>
+                        <li class="nav-item ms-3">
+                            <a href="#" class="btn quote-btn px-3"><span class="leaf-icon"><i class='bx bx-leaf' ></i></span> Get a Quote</a>
+                        </li>
                     </ul>
                     </div>
                 </div>
             </nav>    
-        </div>
-        
+        </div> 
     </header>
     <main>
