c6c19625bdb859e2bda97b4711825f095c8f1498
Daniel Velez <velezdaniel516@gmail.com>
2025-08-11 15:18:02 -0400

    link external css template file to header.php

--- includes/header.php
+++ includes/header.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
     <header>
 
     </header>
     <main>
+        <p>HELLO WORLD</p>
