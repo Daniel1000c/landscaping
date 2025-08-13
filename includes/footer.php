7adca38b01a828cc7a71aee31e67de38a014054d
Daniel Velez <velezdaniel516@gmail.com>
2025-08-13 19:38:09 -0400

    add php echo y date into footer copyright year

--- includes/footer.php
+++ includes/footer.php
@@ -6,47 +6,50 @@
             <div class="row text-md-left">
 
                 <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                    <h5 class="text-uppercase mb-4 font-weight-bold">Company Name</h5>
+                    <h5 class="text-uppercase mb-4 font-weight-bold">Flores Terrain</h5>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi deleniti sequi quisquam ea numquam. Asperiores cum adipisci dicta veniam ex minima eaque assumenda, ipsam quos, eius, corporis eos. Velit, hic!</p>
                 </div>
 
                 <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                    <h5 class="text-uppercase mb-4 font-weight-bold">Products</h5>
+                    <h5 class="text-uppercase mb-4 font-weight-bold">Useful links</h5>
 
                     <p>
-                        <a href="#" style="text-decoration: none;">Products</a>
+                        <a href="#" style="text-decoration: none;">About Us</a>
                     </p>
                     <p>
-                        <a href="#" style="text-decoration: none;">Products</a>
+                        <a href="#" style="text-decoration: none;">Contact us</a>
                     </p>
                     <p>
-                        <a href="#" style="text-decoration: none;">Products</a>
+                        <a href="#" style="text-decoration: none;">FAQ</a>
                     </p>
                     <p>
-                        <a href="#" style="text-decoration: none;">Products</a>
+                        <a href="#" style="text-decoration: none;">Terms & Conditions</a>
                     </p>
                     <p>
-                        <a href="#" style="text-decoration: none;">Products</a>
-                    </p>
-                    <p>
-                        <a href="#" style="text-decoration: none;">Products</a>
+                        <a href="#" style="text-decoration: none;">Privacy Policy</a>
                     </p>
                 </div>
 
                 <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-                    <h5 class="text-uppercase mb-4 font-weight-bold">Useful links</h5>
+                    <h5 class="text-uppercase mb-4 font-weight-bold">Services</h5>
 
                     <p>
-                        <a href="#" style="text-decoration: none;">Your Account</a>
+                        <a href="#" style="text-decoration: none;">Lawn Installation & Maintenance</a>
                     </p>
                     <p>
-                        <a href="#" style="text-decoration: none;">Become an Affiliate</a>
+                        <a href="#" style="text-decoration: none;">Garden Design & Planting</a>
                     </p>
                     <p>
-                        <a href="#" style="text-decoration: none;">Shipping Rates</a>
+                        <a href="#" style="text-decoration: none;">Hardscaping</a>
                     </p>
                     <p>
-                        <a href="#" style="text-decoration: none;">Help</a>
+                        <a href="#" style="text-decoration: none;">Mulching & Soil Conditioning</a>
+                    </p>
+                    <p>
+                        <a href="#" style="text-decoration: none;">Sod Installation</a>
+                    </p>
+                    <p>
+                        <a href="#" style="text-decoration: none;">Seasonal Cleanup</a>
                     </p>
                 </div>
 
@@ -54,16 +57,16 @@
                 <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
                     <h5 class="text-uppercase mb-4 font-weight-bold">Contact</h5>
                     <p>
-                        <i class="fas fa-home mr-3"></i> New York, NY 10012</i>
+                        <i class="fas fa-home mr-3"></i> Cooper City, FL 33001</i>
                     </p>
                     <p>
-                        <i class="fas fa-envelope mr-3"></i> template@gmail.com</i>
+                        <i class="fas fa-envelope mr-3"></i> floresterrain653@gmail.com</i>
                     </p>
                     <p>
-                        <i class="fas fa-phone mr-3"></i> +2423522</i>
+                        <i class="fas fa-phone mr-3"></i> +305 1234 567</i>
                     </p>
                     <p>
-                        <i class="fas fa-print mr-3"></i> +11524215</i>
+                        <i class="fas fa-print mr-3"></i> +305 1234 567</i>
                     </p>
                 </div>
             </div>
@@ -73,9 +76,9 @@
             <div class="row align-items-center">
 
                 <div class="col-md-7 col-lg-8">
-                    <p>Copyright ©2023 All rights reserved by:
+                    <p>Copyright &copy; <?php echo date('Y'); ?> All rights reserved by:
                         <a href="#" style="text-decoration: none;">
-                            <strong>Company Name</strong>
+                            <strong>Flores Terrain</strong>
                         </a>
                     </p>
                 </div>
