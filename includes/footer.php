4ab91d76da71975e5822013308be16c3814d38a0
Daniel Velez <velezdaniel516@gmail.com>
2025-08-13 18:03:24 -0400

    Add footer container, link columns, social media container, and social icons
    -Add template link columns
    -Add footer container to prevent content from stretching into footer
    -Add social media container with social icons

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,108 @@
     </main>
-    <footer>
-        
+    <footer class="pt-5 pb-4">
+
+        <div class="container text-md-left">
+
+            <div class="row text-md-left">
+
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold">Company Name</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi deleniti sequi quisquam ea numquam. Asperiores cum adipisci dicta veniam ex minima eaque assumenda, ipsam quos, eius, corporis eos. Velit, hic!</p>
+                </div>
+
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold">Products</h5>
+
+                    <p>
+                        <a href="#" style="text-decoration: none;">Products</a>
+                    </p>
+                    <p>
+                        <a href="#" style="text-decoration: none;">Products</a>
+                    </p>
+                    <p>
+                        <a href="#" style="text-decoration: none;">Products</a>
+                    </p>
+                    <p>
+                        <a href="#" style="text-decoration: none;">Products</a>
+                    </p>
+                    <p>
+                        <a href="#" style="text-decoration: none;">Products</a>
+                    </p>
+                    <p>
+                        <a href="#" style="text-decoration: none;">Products</a>
+                    </p>
+                </div>
+
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold">Useful links</h5>
+
+                    <p>
+                        <a href="#" style="text-decoration: none;">Your Account</a>
+                    </p>
+                    <p>
+                        <a href="#" style="text-decoration: none;">Become an Affiliate</a>
+                    </p>
+                    <p>
+                        <a href="#" style="text-decoration: none;">Shipping Rates</a>
+                    </p>
+                    <p>
+                        <a href="#" style="text-decoration: none;">Help</a>
+                    </p>
+                </div>
+
+
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold">Contact</h5>
+                    <p>
+                        <i class="fas fa-home mr-3"></i> New York, NY 10012</i>
+                    </p>
+                    <p>
+                        <i class="fas fa-envelope mr-3"></i> template@gmail.com</i>
+                    </p>
+                    <p>
+                        <i class="fas fa-phone mr-3"></i> +2423522</i>
+                    </p>
+                    <p>
+                        <i class="fas fa-print mr-3"></i> +11524215</i>
+                    </p>
+                </div>
+            </div>
+
+            <hr class="mb-4">
+
+            <div class="row align-items-center">
+
+                <div class="col-md-7 col-lg-8">
+                    <p>Copyright ©2023 All rights reserved by:
+                        <a href="#" style="text-decoration: none;">
+                            <strong>Company Name</strong>
+                        </a>
+                    </p>
+                </div>
+
+                <div class="col-md-5 col-lg-4">
+                    <div class="text-center text-md-right">
+                        <ul class="list-unstyled list-inline">
+                            <li class="list-inline-item">
+                                <a href="#" class="btn-floating btn-sm" style="font-size: 23px;"><i><i class="fab fa-facebook-f"></i></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#" class="btn-floating btn-sm" style="font-size: 23px;"><i><i class="fab fa-twitter"></i></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#" class="btn-floating btn-sm" style="font-size: 23px;"><i><i class="fab fa-instagram"></i></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#" class="btn-floating btn-sm" style="font-size: 23px;"><i><i class="fab fa-youtube"></i></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 </body>
