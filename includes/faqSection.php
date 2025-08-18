c8c2b6fb1fba06a497ad02a82b42f4693210f302
Daniel Velez <velezdaniel516@gmail.com>
2025-08-18 16:38:07 -0400

    Reformat accordion items and Rewrite faq questions
    -Rewrite faq questions to be more shorter
    -Reformat accordion items to be spaced out more for readability

--- includes/faqSection.php
+++ includes/faqSection.php
@@ -4,66 +4,86 @@
 
         <!--FAQ Section-->
         <div class="accordion" id="accordionExample">
-        <div class="accordion-item">
-            <h2 class="accordion-header">
-            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                What types of landscaping Services do you offer?
-            </button>
-            </h2>
-            <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-            <div class="accordion-body">
-                We offer a wide range of services, including lawn care, garden design, hardscaping (patios, walkways), tree and shrub planting, irrigation installation, and more.
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    What services are offered?
+                    <span class="accordion-icon">
+                        <i class="fas fa-tint"></i>
+                    </span>
+                </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                       &#8226; "We offer a wide range of services, including lawn care, garden design, hardscaping (patios, walkways), tree and shrub planting, irrigation installation, and more."
+                    </div>
+                </div>
             </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    How do I get started with a project?
+                    <span class="accordion-icon">
+                        <i class="fas fa-tint"></i>
+                    </span>
+                </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                      &#8226; "Simply contact us for a free consultation. We’ll assess your space, discuss your vision, and create a customized plan that fits your needs and budget."
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                How do I get started with landscaping project?
-            </button>
-            </h2>
-            <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-            <div class="accordion-body">
-                Simply contact us for a free consultation. We’ll assess your space, discuss your vision, and create a customized plan that fits your needs and budget.
+
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Do you offer maintenance services?
+                    <span class="accordion-icon">
+                        <i class="fas fa-tint"></i>
+                    </span>
+                </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        &#8226; "Yes! We provide regular lawn mowing, fertilization, weed control, and seasonal clean-up to keep your lawn looking its best year-round."
+                    </div>
+                </div>
             </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                    Can you help design a plan for my property?
+                    <span class="accordion-icon">
+                        <i class="fas fa-tint"></i>
+                    </span>
+                </button>
+                </h2>
+                <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        &#8226; "Absolutely! Our design team will work with you to create a landscape plan that suits your style, needs, and the unique features of your property."
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Do you offer lawn maintenance services?
-            </button>
-            </h2>
-            <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-            <div class="accordion-body">
-                Yes! We provide regular lawn mowing, fertilization, weed control, and seasonal clean-up to keep your lawn looking its best year-round.
+
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                    What's the best time of the year to start a project?
+                    <span class="accordion-icon">
+                        <i class="fas fa-tint"></i>
+                    </span>
+                </button>
+                </h2>
+                <div id="collapseFive" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                       &#8226; "Spring and early fall are ideal times for landscaping, as the weather is mild, and plants establish roots before extreme temperatures hit."
+                    </div>
+                </div>
             </div>
-            </div>
-        </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                Can you help design a landscaping plan for my property?
-            </button>
-            </h2>
-            <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-            <div class="accordion-body">
-               Absolutely! Our design team will work with you to create a landscape plan that suits your style, needs, and the unique features of your property.
-            </div>
-            </div>
-        </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-                What's the best time of the year to start a landscaping project?
-            </button>
-            </h2>
-            <div id="collapseFive" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-            <div class="accordion-body">
-                Spring and early fall are ideal times for landscaping, as the weather is mild, and plants establish roots before extreme temperatures hit.
-            </div>
-            </div>
-        </div>
+
         </div>
     </div>
 </section>
